80e3cfa684850c36b47197706cf1fe1bd1be6378
vesk4000 <veskmail@gmail.com>
2023-12-03 10:11:58 +0100

    Today the problem wasn't very hard, it stumped me for a second, but I thought of the solution almost instantly. Implementing it though was another story... `usize` and subtracting don't play well, apparently. Nor, do `i32` and indexing. And I just found out, there are panics which you cannot catch. You learn something every day I guess; especially when you started learning Rust two days ago...
    P.S: Why does `map[x]` return an immutable reference? The `.insert` reminds me of Java (yuck). Doesn't make sense to me, they should make it consistent with `Vec`.

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -11,20 +11,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let re = Regex::new(r"\d+")?;
     let symbols = HashSet::from(['$', '%', '/', '*', '&', '#', '+', '-', '@', '=']);
     let mut ans = 0;
+    let mut ans2 = 0;
+    let mut v = HashMap::<(usize, usize), (usize, i32)>::new();
     for (i, line) in lines.iter().enumerate() {
         for _match in re.find_iter(line) {
             'curr_num: for y in i.checked_sub(1).unwrap_or(0)..=min(lines.len() - 1, i + 1) {
                 for x in _match.start().checked_sub(1).unwrap_or(0)..=min(line.len() - 1, _match.end()) {
                     if(symbols.contains(&lines[y].chars().nth(x).unwrap())) {
                         //println!("{}", _match.as_str());
-                        ans += _match.as_str().parse::<i32>()?;
-                        break 'curr_num;
+                        if(lines[y].chars().nth(x).unwrap() == '*') {
+                            if(!v.contains_key(&(x, y))) {
+                                v.insert((x, y), (0, 1));
+                            }
+                            let (count, sum) = v[&(x, y)];
+                            v.insert((x, y), (count + 1, sum * _match.as_str().parse::<i32>()?));
+                        }
+                        //ans += _match.as_str().parse::<i32>()?;
+                        //break 'curr_num;
                     }
                 }
             }
         }
     }
+
     println!("{ans}");
+    println!("{}", v.values().map(|vv| if vv.0 == 2 {vv.1} else {0}).sum::<i32>());
     Ok(())
 }
 
