cc71dafcfaa18dffbd698e1f28999b27e4e156af
vesk4000 <veskmail@gmail.com>
2023-12-06 09:12:17 +0100

    Pretty easy one today. Had I not done yesterday's puzzle, I would've thought today's Part 2 wouldn't be doable using brute force, but it is, even though the numbers are `u64`s. Absolutely no clue why I had to use `&*times_line` in lines 10 and 11, but Jetbrains luckily helped with that a lot.

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -4,12 +4,14 @@ use regex::Regex;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = fs::read_to_string("inputs/day6.txt")?;
     let (time_line, dist_line) = input.trim().split_once("\n").unwrap();
+    let times_line = time_line.replace(" ", "");
+    let dists_line = dist_line.replace(" ", "");
     let re = Regex::new(r"\d+")?;
-    let times = re.find_iter(time_line).map(|m| m.as_str().parse::<u32>().unwrap());
-    let dists = re.find_iter(dist_line).map(|m| m.as_str().parse::<u32>().unwrap());
-    let mut ans = 1;
+    let times = re.find_iter(&*times_line).map(|m| m.as_str().parse::<u64>().unwrap());
+    let dists = re.find_iter(&*dists_line).map(|m| m.as_str().parse::<u64>().unwrap());
+    let mut ans: i64 = 1;
     for (time, best_dist) in times.zip(dists) {
-        let mut num = 0;
+        let mut num: i64 = 0;
         for i in 0..=time {
             if i * (time - i) > best_dist {
                 num += 1;
