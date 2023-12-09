c5c5b977334ae0c1a30c81c9aebc0a7b972364e9
vesk4000 <veskmail@gmail.com>
2023-12-09 07:37:08 +0100

    Quite an easy one today. If only I hadn't forgotten that my regex for parsing all the numbers in a line (really useful btw) didn't include minus signs... Anyway, I'm starting to enjoy Rust a bit, no real issues today. I really liked how I could change a Vec<i32> into a &[i32] without having to change anything else in the code.

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -7,23 +7,26 @@ fn main() {
     let lines = input.trim().split("\n");
     let re = Regex::new(r"-?\d+").unwrap();
     let mut ans = 0;
+    let mut ans_pt2 = 0;
     for line in lines {
         let nums = re.find_iter(line).map(|m| m.as_str().parse::<i32>().unwrap()).collect::<Vec<_>>();
-        ans += solve(&nums);
-        println!("{}", solve(&nums));
+        let sol = solve(&nums);
+        ans += sol.1;
+        ans_pt2 += sol.0;
     }
-    println!("{ans}");
+    println!("{ans} {ans_pt2}");
 }
 
-fn solve(v: &[i32]) -> i32 {
-    if v.iter().all(|x| *x == 0) { return 0; }
+fn solve(v: &[i32]) -> (i32, i32) {
+    if v.iter().all(|x| *x == 0) { return (0, 0); }
 
     let mut vv = Vec::<i32>::new();
     for i in 0..v.len() - 1 {
         vv.push(v[i + 1] - v[i]);
     }
 
-    v[v.len() - 1] + solve(&vv)
+    let sol = solve(&vv);
+    (v[0] - sol.0, v[v.len() - 1] + sol.1)
 }
 
 static EXAMPLE: &str = "
